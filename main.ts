a5d31d3de78a7fe613f1b2d77fe33821c41ce065
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2023-05-19 12:04:31 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+let n = 0
+input.onGesture(Gesture.Shake, function () {
+    n = randint(1, 6)
+    basic.showNumber(n)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
